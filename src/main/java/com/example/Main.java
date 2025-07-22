6534372d32fea14f2f0b7e8912a4daef62dc9eb9
Shekhar <shekharw2298@gmail.com>
2025-07-22 16:14:18 +0530

    testing added

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,17 +11,15 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws IOException, SQLException {
-        // ✅ Start H2 Web Console (default port 8082)
+
         Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
         System.out.println("H2 Console is running at http://localhost:8082");
 
-        // ✅ Start TCP server (if you use tcp:// in JDBC)
+
         Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
 
-        // Init DB
         DatabaseManager.initDatabase();
 
-        // Start HTTP server
         HttpServer server1 = server.createServer(8000);
         server1.start();
         System.out.println("Server running at http://localhost:8000");
